8a5f40cbf09f682cc503887502658cf441113d33
deena <deenathayalan947@gmail.com>
2025-03-17 09:36:23 +0530

    init commit

---
+++ lib/screens/landingscreen.dart
@@ -0,0 +1,101 @@
+import 'package:adrox/screens/signinscreen.dart';
+import 'package:adrox/utils/text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+
+  void signIn(){
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>
+    const SignInScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body:Container(
+        height: double.infinity.h,
+        width: double.infinity.w,
+        padding: EdgeInsets.only(bottom: 20.h),
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: Align(
+          alignment: Alignment.bottomCenter, // Adjust as needed
+          child: Container(
+            margin: EdgeInsets.only(top: 200.h), // Equivalent to top: 200.h in Positioned
+            padding: EdgeInsets.all(20.h),
+            height: 320.h,
+            width: 320.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.r),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  StringValues.landingTitle,
+                  style: TextStyle(
+                    fontFamily: "Roboto-bold",
+                    fontSize: 31.sp,
+                  ),
+                ),
+                Text(
+                  StringValues.landingCont,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: "Roboto-normal",
+                    fontSize: 12.sp,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(150.w, 40.h),
+                        backgroundColor: const Color(0xff12172C),
+                      ),
+                      child: Text(
+                        "Create Wallet",
+                        style: TextStyle(
+                          fontFamily: "Roboto-normal",
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        signIn();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(100.w, 40.h),
+                        backgroundColor: const Color(0xff3F5FF2),
+                      ),
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontFamily: "Roboto-normal",
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ),
+    ));
+  }
+}
---
+++ lib/screens/signinscreen.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../utils/text.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+
+  final walletAddress = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body:Container(
+          height: double.infinity.h,
+          width: double.infinity.w,
+          padding: EdgeInsets.only(bottom: 20.h),
+          decoration: const BoxDecoration(color: Colors.blue),
+          child: Align(
+            alignment: Alignment.bottomCenter, // Adjust as needed
+            child: Container(
+              margin: EdgeInsets.only(top: 200.h), // Equivalent to top: 200.h in Positioned
+              padding: EdgeInsets.all(25.h),
+              height: 520.h,
+              width: 320.w,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.r),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    StringValues.loginTitle,
+                    style: TextStyle(
+                      fontFamily: "Roboto-bold",
+                      fontSize: 25.sp,
+                    ),
+                  ),
+                  Text(
+                    StringValues.loginCont,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontFamily: "Roboto-lite",
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w400
+                    ),
+                  ),
+                  Text(
+                    "Wallet Address",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Roboto-normal",
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  TextField(
+                    cursorColor: Colors.transparent,
+                    controller: walletAddress,
+                    decoration: InputDecoration(
+                      hintText: "       Enter Wallet Address",
+                        hintStyle: TextStyle(
+                      color: Colors.black45, fontFamily: "Roboto-lite", fontSize: 14.sp,
+                    ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 20.h),
+                      enabledBorder: OutlineInputBorder(borderSide: const BorderSide(
+                          color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                          color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r)),
+                      border: OutlineInputBorder(borderSide: const BorderSide(
+                        color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r))
+                    ),
+                  ),
+                  Text(
+                    "Mnemonic Key",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Roboto-normal",
+                      fontSize: 14.sp,
+                    ),
+                  ),
+                  TextField(
+                    cursorColor: Colors.transparent,
+                    controller: walletAddress,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 28.h),
+                      enabledBorder: OutlineInputBorder(borderSide: const BorderSide(
+                          color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                          color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r)),
+                      border: OutlineInputBorder(borderSide: const BorderSide(
+                        color: Colors.grey
+                      ),borderRadius: BorderRadius.circular(20.r))
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(100.w, 40.h),
+                          backgroundColor: const Color(0xffF4F4F6),
+                        ),
+                        child: Text(
+                          "Back",
+                          style: TextStyle(
+                            fontFamily: "Roboto-normal",
+                            fontSize: 16.sp,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(150.w, 40.h),
+                          backgroundColor: const Color(0xff3F5FF2),
+                        ),
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            fontFamily: "Roboto-normal",
+                            fontSize: 16.sp,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+      ),
+    ));
+  }
+}
